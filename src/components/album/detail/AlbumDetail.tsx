e6ce1957204334773ca74be056bccc0705f7cb27
John Han <work.johnhan@gmail.com>
2025-02-04 01:09:38 +0900

    Feat: AlbumDetail Simplify MODE

--- src/components/album/detail/AlbumDetail.tsx
+++ src/components/album/detail/AlbumDetail.tsx
@@ -10,10 +10,15 @@ export default function AlbumDetail({ album }: { album: Album }) {
   const [isSimplify, setIsSimplify] = useState(false);
   const simplifyAlbumInfo = () => {
     setIsSimplify(!isSimplify);
+    console.log(isSimplify);
   };
 
   return (
-    <div className="absolute z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[770px] h-[440px] rounded-2xl backdrop-blur-md bg-white/40 p-[30px] flex flex-col gap-y-3">
+    <div
+      className={`absolute transition-all duration-500 z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl backdrop-blur-md bg-white/40 flex flex-col gap-y-3 ${
+        isSimplify ? "w-[300px] p-[20px] " : "w-[770px] h-[440px] p-[30px] "
+      }`}
+    >
       <div className="flex justify-center items-end relative">
         <div className="mr-auto gap-x-[20px] flex absolute top-0 left-0">
           <Link
@@ -31,29 +36,39 @@ export default function AlbumDetail({ album }: { album: Album }) {
             className="w-[20px] h-[20px] bg-window-green rounded-full"
           ></div>
         </div>
-        <p className="font-pretendard font-medium text-primary text-xl -translate-y-1">
+        <p
+          className={`${isSimplify ? "hidden" : "block"} font-pretendard font-medium text-primary text-xl -translate-y-1`}
+        >
           {album.date}
         </p>
       </div>
-      <div className="flex gap-x-[30px]">
+      <div
+        className={`flex gap-x-[30px] ${isSimplify ? "flex-col translate-y-[20px]" : ""}`}
+      >
         <Link href={album.rcLink} target="_blank" rel="noopener noreferrer">
           <Image
             src={album.src}
             alt="album"
-            width={340}
-            height={340}
+            width={isSimplify ? 260 : 340}
+            height={isSimplify ? 260 : 340}
             className="rounded-2xl"
           />
         </Link>
         <div className="flex flex-col w-[340px]">
-          <p className="text-[20px] font-semibold font-pretendard text-black">
+          <p
+            className={`${isSimplify ? "mt-1" : ""} text-[20px] font-semibold font-pretendard text-black`}
+          >
             {album.title}
           </p>
 
-          <p className="text-[18px] font-regular font-pretendard text-black line-clamp-4 mt-1">
+          <p
+            className={`${isSimplify ? "hidden" : "line-clamp-4"} text-[18px] font-regular font-pretendard text-black mt-1`}
+          >
             {album.description}
           </p>
-          <div className="w-full h-[1px] bg-white my-3"></div>
+          <div
+            className={`${isSimplify ? "w-[260px]" : "w-full"} h-[1px] bg-white my-3`}
+          ></div>
           {album.list.map((item, index) => (
             <div
               className="flex gap-x-[15px] items-center justify-start"
@@ -77,6 +92,9 @@ export default function AlbumDetail({ album }: { album: Album }) {
           ))}
         </div>
       </div>
+      <div
+        className={`${isSimplify ? "block" : "hidden"} w-full h-[1px]`}
+      ></div>
     </div>
   );
 }
