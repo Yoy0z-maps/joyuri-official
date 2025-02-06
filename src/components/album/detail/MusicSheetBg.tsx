7bb7aeb71e51274868260cf77412d173705c45a1
John Han <work.johnhan@gmail.com>
2025-02-06 17:58:00 +0900

    Feat: resolve ESLint

--- src/components/album/detail/MusicSheetBg.tsx
+++ src/components/album/detail/MusicSheetBg.tsx
@@ -7,12 +7,14 @@ export default function MusicSheetBg({ album }: { album: Album }) {
   return (
     <div className="relative w-[100%] h-[100vh] bg-music-sheet">
       <div className="absolute bottom-0 left-0">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           src="/assets/images/album/details/treble_clef.png"
           alt="music-sheet"
         />
       </div>
       <div className="absolute top-0 right-0">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           src="/assets/images/album/details/bass_clef.png"
           alt="music-sheet"
